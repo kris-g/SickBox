e50ac4212ebbe78a8aa47262f2ed13825a04d4e2
KrisG <git@krisg.co.uk>
2016-03-06 18:32:36 +0000

    Removing WebStreamProvider timeout.

--- Src/KrisG.Utility/Interfaces/Web/IWebStreamProvider.cs
+++ Src/KrisG.Utility/Interfaces/Web/IWebStreamProvider.cs
@@ -1,11 +1,9 @@
-﻿using System;
-using System.IO;
+﻿using System.IO;
 
 namespace KrisG.Utility.Interfaces.Web
 {
     public interface IWebStreamProvider
     {
-        TimeSpan Timeout { get; set; }
         Stream GetStream(string url);
     }
 }
--- Src/KrisG.Utility/Web/WebStreamProvider.cs
+++ Src/KrisG.Utility/Web/WebStreamProvider.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.IO;
+﻿using System.IO;
 using System.Net;
 using KrisG.Utility.Interfaces.Web;
 
@@ -7,17 +6,9 @@ namespace KrisG.Utility.Web
 {
     public class WebStreamProvider : IWebStreamProvider
     {
-        public TimeSpan Timeout { get; set; }
-
-        public WebStreamProvider()
-        {
-            Timeout = TimeSpan.FromSeconds(10);
-        }
-
         public Stream GetStream(string url)
         {
             var webRequest = WebRequest.Create(url);
-            webRequest.Timeout = (int) Timeout.TotalMilliseconds;
             var webStream = webRequest.GetResponse().GetResponseStream();
             return webStream;
         }
